4c2379dcdc43c75a7ef306f7c8a7a34c8487abb7
David Aronchick <aronchick@gmail.com>
2021-11-23 00:38:02 +0000

    qr scanning and capture working

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-let express = require('express');
+let express = require("express");
 let app = express();
-let ejs = require('ejs');
-var fs = require('fs');
+let ejs = require("ejs");
+var fs = require("fs");
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'))
-app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get('/.well-known/acme-challenge/pu-xAQgUOqiseeLl506GV2CZ4UeiPFcLnzH5nZI-x9U', (req, res) => {
-  fs.readFile('/home/nodejs/domainchallenge.txt', (e, data) => {
-    if (e) throw e;
-    res.send(data);
-  });
-});
+// app.get('/.well-known/acme-challenge/pu-xAQgUOqiseeLl506GV2CZ4UeiPFcLnzH5nZI-x9U', (req, res) => {
+//   fs.readFile('/home/nodejs/domainchallenge.txt', (e, data) => {
+//     if (e) throw e;
+//     res.send(data);
+//   });
+// });
 
-app.post('')
+app.post("");
 
 module.exports = app.listen(port);
