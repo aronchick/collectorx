2f7244cc7cb376b265beee7c5ae1f2926eca4368
David Aronchick <aronchick@gmail.com>
2021-11-30 02:26:34 +0000

    new port and serving

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,11 +1,12 @@
 // vue.config.js
 module.exports = {
   devServer: {
+    disableHostCheck: true,   // That solved it
     overlay: {
       warnings: true,
       errors: true
     },
     'proxy': "http://127.0.0.1"
   },
-productionSourceMap: true,
+  productionSourceMap: true,
 }
